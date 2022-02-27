76658601cc94407f6cd1785b166e75e6698c1261
Kubitssu <kubitssu@gmail.com>
2022-02-27 19:33:39 +0100

    first adjustments

---
+++ lua/qbeetsu/globals.lua
@@ -0,0 +1,2 @@
+vim.g['transparent_enabled'] = true
+vim.g['gruvbox_transparent_bg'] = 1
---
+++ lua/qbeetsu/tabnine.lua
@@ -0,0 +1,5 @@
+local status_ok, tabnine = pcall(require, "cmp_tabnine.config")
+if not status_ok then
+  return
+end
+
